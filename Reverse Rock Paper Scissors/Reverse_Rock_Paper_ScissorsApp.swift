d9b011d6d5a04d2dd508a80584c8b769b1b7016b
mcantop <maciejcantop@icloud.com>
2022-07-25 17:41:03 +0200

    Initial Commit

---
+++ Reverse Rock Paper Scissors/Reverse_Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reverse_Rock_Paper_ScissorsApp.swift
+//  Reverse Rock Paper Scissors
+//
+//  Created by Maciej on 25/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Reverse_Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
